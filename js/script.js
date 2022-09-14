24ee10bc6854b89a0ed8f4e2e05f71879c35519e
Denis-92 <d.mozenic@gmail.com>
2022-09-14 19:09:58 +0200

    aggiunto timer scorrimento automatico

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
 console.log('JS OK');
 
 const slides = [
+
     {
         image: 'img/01.jpg',
         title: 'Svezia',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     },
+
     {
         image: 'img/02.jpg',
         title: 'Svizzera',
         text: 'Lorem ipsum.',
     },
+
     {
         image: 'img/03.jpg',
         title: 'Gran Bretagna',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     },
+
     {
         image: 'img/04.jpg',
         title: 'Germania',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
     },
+
     {
         image: 'img/05.jpg',
         title: 'Paradise',
         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
     }
+
 ];
-//console.log(slides.length);
 
 
 const app = new Vue(
@@ -42,6 +47,8 @@ const app = new Vue(
 
             selection: 0,
 
+            intervalID: undefined
+
         },
 
         methods: {
@@ -53,6 +60,9 @@ const app = new Vue(
                 } else {
                     this.selection--;
                 }
+                this.stopInterval();
+                this.makeInterval();
+
             },
 
             scrollDown() { // ': function' NON NECESSARIO
@@ -62,13 +72,43 @@ const app = new Vue(
                 } else {
                     this.selection = 0;
                 }
+                this.stopInterval();
+                this.makeInterval();
+
             },
 
             imagePointer(pointSelection) {
 
                 this.selection = pointSelection;
 
+            },
+
+            makeInterval() {
+
+                this.intervalID = setInterval(this.scrollDown, 3000);
+
+            },
+
+            stopInterval() {
+
+                if (this.intervalID) {
+                    clearInterval(this.intervalID);
+                }
+
             }
+
+        },
+
+        // created() {
+
+        //     console.log('created!');
+
+        // },
+
+        mounted() {
+
+            console.log('mounted!');
+            this.makeInterval();
 
         }
 
